b384c73d767501d853b1b4808fd027768b938a17
Gayatri Patil <157716004+Gayatri120@users.noreply.github.com>
2025-03-24 11:09:46 +0530

    WEB CLASSROOM

---
+++ about.php
@@ -0,0 +1,217 @@
+        <?php include('allhead.php'); ?>
+        <!-- Page Content -->
+        <div class="container" style="max-width: 1200px;">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+        <div class="col-lg-12">
+        <h1 class="page-header">About
+        <small>WebClassroom</small>
+        </h1>
+        <ol class="breadcrumb">
+        <li><a href=" index">Home</a>
+        </li>
+        <li class="active">About</li>
+     </ol>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row">
+        <div class="col-md-6">
+        <img class="img-responsive" src="images/T1.jpeg" alt="">
+        </div>
+        <div class="col-md-6">
+        <h2>ABOUT WebClassroom </h2>
+        <p>The <strong>“WebClassroom”</strong> Website (web based application) is useful for the
+         students, faculty, guest who ever likes to learn from web using E-Learn
+            (Videos), as well Check result, schedules of assessment and all that task like
+            event, news, students can find out list of fresh courses offered by them and
+               admission procedure, discussion forum, fee structure etc. without going to
+                     institute. It provides the facility to the students or guest to have complete
+                           information about the institute.</p><br>
+<p>
+In this application, student can attend his\her missed classes from e-learn.</p>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Team Members -->
+        <div class="row">
+        <div class="col-lg-12">
+        <h2 class="page-header">Our Alumni</h2>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/a7.jpeg" alt="">
+        <div class="caption">
+        <h3>Ritu Gaikwad<br>
+            <small>Teacher</small>
+        </h3>
+        <p>Our Computer teacher is my favorite teacher..</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/a5.jpeg" alt="">
+        <div class="caption">
+        <h3>Kunal patil<br>
+            <small>Teacher</small>
+        </h3>
+        <p>Because of my Science Teacher, Science has become my favorite subject too..</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/a1.jpeg" alt="">
+        <div class="caption">
+        <h3>Sagar umbaraj<br>
+            <small>Teacher</small>
+        </h3>
+        <p>No one in our class has failed in Science because of his teaching.</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/a2.jpeg" alt="">
+        <div class="caption">
+        <h3>Sumita Thakur<br>
+            <small>Teacher</small>
+        </h3>
+        <p>Our Science teacher is my favorite teacher.</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/a3.jpeg" alt="">
+        <div class="caption">
+        <h3>Rachna mali<br>
+            <small>Teacher</small>
+        </h3>
+        <p>He is very kind to all the students.</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/a4.jpeg" alt="">
+        <div class="caption">
+        <h3>Chaitali nair<br>
+            <small>Teacher</small>
+        </h3>
+        <p>He teaches the lessons every day and keeps tests regularly.</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Our Customers -->
+        <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header">Our Student's</h2>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/welcome46.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/smilingFemaleStudent.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/Returning-Student.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/Hopeful_student_1.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/happy_student4.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/library_guy_900x400.jpg" alt="">
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+        <!-- Footer -->
+        <footer>
+
+        <p class="footer text-center"> WebClassroom <strong>BSC(CS)</strong> project Made by <strong>GAYATRI PATIL & TUSHAR PAWAR</strong></p>
+
+        </footer>
+
+        </div>
+        <!-- /.container -->
+        <style>
+
+        .footer{background: #000; padding: 10px 0px; color: #fff;position: fixed;left: 0; right: 0;bottom: -10px;}
+
+        </style>
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Script to Activate the Carousel -->
+        <script>
+        $('.carousel').carousel({
+        interval: 5000 //changes the speed
+        })
+        </script>
+
+        </body>
+        </html>
